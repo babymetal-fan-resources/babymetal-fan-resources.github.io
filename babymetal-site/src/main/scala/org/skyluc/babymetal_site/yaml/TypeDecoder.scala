220d574a1052a8f051e10eec2d1d1942ddeb6890
Luc Bourlier <lb@skyluc.org>
2025-07-31 07:59:18 +0000

    Added wiki and choreographer to existing entries

--- babymetal-site/src/main/scala/org/skyluc/babymetal_site/yaml/TypeDecoder.scala
+++ babymetal-site/src/main/scala/org/skyluc/babymetal_site/yaml/TypeDecoder.scala
@@ -1,5 +1,33 @@
 package org.skyluc.babymetal_site.yaml
 
+import org.skyluc.fan_resources.data.AttributeDefinition
+import org.skyluc.fan_resources.html.ElementInfo
 import org.skyluc.fan_resources.yaml.FrDecoders
 
-object BabymetalSiteDecoders extends FrDecoders {}
+import YamlKeys.*
+
+object BabymetalSiteDecoders extends FrDecoders {
+
+  val ATTRIBUTE_DEFINITION_WIKI =
+    AttributeDefinition(
+      WIKI,
+      "BABYMETAL Wiki",
+      true,
+      ElementInfo.INFO_LEVEL_ALL,
+    )
+
+  override def albumAttributeDefinitions: Seq[AttributeDefinition] = Seq(
+    ATTRIBUTE_DEFINITION_WIKI
+  )
+
+  override def songAttributeDefinitions: Seq[AttributeDefinition] = Seq(
+    AttributeDefinition(
+      CHOREOGRAPHER,
+      "choreographer",
+      false,
+      ElementInfo.INFO_LEVEL_ALL,
+    ),
+    ATTRIBUTE_DEFINITION_WIKI,
+  )
+
+}
--- babymetal-site/src/main/scala/org/skyluc/babymetal_site/yaml/YamlKeys.scala
+++ babymetal-site/src/main/scala/org/skyluc/babymetal_site/yaml/YamlKeys.scala
@@ -2,4 +2,6 @@ package org.skyluc.babymetal_site.yaml
 
 object YamlKeys {
   val CHRONOLOGYPAGE = "chronologypage"
+  val CHOREOGRAPHER = "choreographer"
+  val WIKI = "wiki"
 }
